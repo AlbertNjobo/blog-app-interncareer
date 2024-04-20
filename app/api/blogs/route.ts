57ed8964e6436c86b083bbeac62ec92f9bf509ec
Albert Njobo <n02212525l@students.nust.ac.zw>
2024-04-20 10:29:31 +0200

    Update route.ts

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -1,26 +1,24 @@
-import {NextResponse}  from "next/server"
-import primsa from "../../lib/prismadb"
-
+import { NextResponse } from "next/server";
+import prisma from "../../lib/prismadb";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-export async function POST(
-    request: Request,
-     ){
-        const currentUser = await getCurrentUser();
+export async function POST(request: Request) {
+  const currentUser = await getCurrentUser();
 
-        if (!currentUser){
-            return null
-        }
-        const body = await request.json();
+  if (!currentUser) {
+    return new NextResponse(null, { status: 401 });
+  }
 
-        const {name, description, imageSrc} = body
+  const body = await request.json();
 
-        const blog = await prisma?.blog.create({
-            data: {name, 
-                description, 
-                imageSrc,
-            userId: currentUser.id}
-        })
-        return NextResponse.json(blog)
-     }
+  const { name, description, imageSrc } = body;
 
+  const blog = await prisma?.blog.create({
+    data: { name, description, imageSrc, userId: currentUser.id },
+  });
+
+  return new NextResponse(JSON.stringify(blog), {
+    status: 201,
+    headers: { "Content-Type": "application/json" },
+  });
+}
