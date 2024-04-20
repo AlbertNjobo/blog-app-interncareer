525424e7126bb38aa8e98c6b5b40609bb314cfd1
Albert Njobo <n02212525l@students.nust.ac.zw>
2024-04-20 10:13:24 +0200

    Update route.ts

--- app/api/blogs/route.ts
+++ app/api/blogs/route.ts
@@ -10,39 +10,35 @@ interface BlogPostRequestBody {
 }
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // Ensure this handler only responds to POST requests
-  if (req.method !== 'POST') {
-    return res.status(405).end(); // Method Not Allowed
+    req: NextApiRequest,
+    res: NextApiResponse
+  ) {
+    if (req.method !== 'POST') {
+      return res.status(405).end(); // Method Not Allowed
+    }
+  
+    const currentUser = await getCurrentUser();
+  
+    if (!currentUser) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+  
+    // Access the request body directly, assuming it's already parsed as JSON.
+    const { name, description, imageSrc } = req.body;
+  
+    try {
+      const blog = await prisma.blog.create({
+        data: {
+          name,
+          description,
+          imageSrc,
+          userId: currentUser.id,
+        },
+      });
+  
+      return res.status(200).json(blog);
+    } catch (error) {
+      console.error('Failed to create blog post:', error);
+      return res.status(500).json({ error: 'Failed to create blog post' });
+    }
   }
-
-  const currentUser = await getCurrentUser();
-
-  if (!currentUser) {
-    // If no current user is found, return a 401 Unauthorized response.
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
-  try {
-    const body: BlogPostRequestBody = await req.json();
-    const { name, description, imageSrc } = body;
-
-    const blog = await prisma.blog.create({
-      data: {
-        name,
-        description,
-        imageSrc,
-        userId: currentUser.id
-      }
-    });
-
-    // Return the created blog post as a JSON response with a 200 OK status.
-    return res.status(200).json(blog);
-  } catch (error) {
-    // Handle any errors that occur during the request processing.
-    console.error('Failed to create blog post:', error);
-    return res.status(500).json({ error: 'Failed to create blog post' });
-  }
-}
